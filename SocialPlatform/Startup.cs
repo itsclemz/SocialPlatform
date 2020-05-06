a3ba1d6ad15b69032d8b10c5bdd5fce0972ce16b
Isaac Clement <isaacclem7@gmail.com>
2020-05-06 12:30:20 +0100

    Add project files.

---
+++ SocialPlatform/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialPlatform.Startup))]
+namespace SocialPlatform
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
