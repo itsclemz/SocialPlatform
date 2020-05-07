bc9f592546fd92d2057c2dd46aacfb2a6ff219bd
Isaac Clement <isaacclem7@gmail.com>
2020-05-07 05:10:00 +0100

    chat, email, music, profile pic, profile video was added

---
+++ SocialPlatform/Controllers/ProfilePictureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialPlatform.Controllers
+{
+    public class ProfilePictureController : Controller
+    {
+        // GET: ProfilePicture
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
